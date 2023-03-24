b1553db6563cf7b46338faed7b23802f6cbae391
Dionatan <dionatanonliveira@cidadaoon.com>
2023-03-24 16:51:22 -0300

    site finalizado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,7 @@ import { navLinks } from '../constants'
 
 const NavBar = () => {
   const [toggle, setToggle] = useState(false);
-
-
+  
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
